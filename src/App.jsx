edb7b2d7a64d916a7ee0d526bf96133743264b80
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-03-06 11:54:17 +0530

    Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './components/NavBar.component'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+import Me from './pages/Me.page'
+import Projects from './pages/Projects.page';
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Me/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+import "../style/NavBar.css";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li><Link to="/">Me</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
