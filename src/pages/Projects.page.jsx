d1c4da206815d85131ae213e27e231ed61fbeb3b
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-03-27 17:05:40 +0530

    Cln Up

--- src/pages/Projects.page.jsx
+++ src/pages/Projects.page.jsx
@@ -1,5 +1,5 @@
 import "../styles/Project.style.css";
-
+import {projects} from '../db/projects.data'
 const PortfolioCard = ({ title, description, image, link }) => {
   return (
     <div className="portfolio-card">
@@ -17,15 +17,8 @@ const PortfolioCard = ({ title, description, image, link }) => {
   );
 };
 
+
 const ProjectsPage = () => {
-  const projects = [
-    {
-      title: "📊 Pizza Sales Analysis: Key Insights & Business Strategies 🍕",
-      description: "Analyzing 49,574 pizza sales records to uncover trends in size, type, price, and peak sales times. Discover the most popular pizza sizes, top-selling price ranges, and peak business hours. Gain data-driven recommendations to optimize pricing, promotions, and customer preferences for increased sales. 🚀",
-      image: "https://res.cloudinary.com/ddpvlh9he/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,l_text:montserrat_25_style_light_align_center:Shop%20Now,w_0.5,y_0.18/v1743074919/DALL_E_2025-03-27_16.57.20_-_A_visually_engaging_cover_image_for_a_pizza_sales_analysis_report._The_image_features_a_dynamic_data-driven_theme_with_elements_representing_sales_ins_lnsxwe.webp",
-      link: "https://github.com/SayidMushtaQ/PizzaSales",
-    },
-  ];
 
   return (
     <div className="projects-page">
