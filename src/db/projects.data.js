1263eeb6898acc3fadd3801a59b11edd0ca3fe7a
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-04-11 21:01:25 +0530

    Boston house price - Project 2

--- src/db/projects.data.js
+++ src/db/projects.data.js
@@ -2,7 +2,13 @@ export const projects = [
     {
       title: "📊 Pizza Sales Analysis: Key Insights & Business Strategies 🍕",
       description: "Analyzing 49,574 pizza sales records to uncover trends in size, type, price, and peak sales times. Discover the most popular pizza sizes, top-selling price ranges, and peak business hours. Gain data-driven recommendations to optimize pricing, promotions, and customer preferences for increased sales. 🚀",
-      image: "https://res.cloudinary.com/ddpvlh9he/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,l_text:montserrat_25_style_light_align_center:Shop%20Now,w_0.5,y_0.18/v1743074919/DALL_E_2025-03-27_16.57.20_-_A_visually_engaging_cover_image_for_a_pizza_sales_analysis_report._The_image_features_a_dynamic_data-driven_theme_with_elements_representing_sales_ins_lnsxwe.webp",
+      image: "https://res.cloudinary.com/ddpvlh9he/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,w_0.5,y_0.18/v1743074919/DALL_E_2025-03-27_16.57.20_-_A_visually_engaging_cover_image_for_a_pizza_sales_analysis_report._The_image_features_a_dynamic_data-driven_theme_with_elements_representing_sales_ins_lnsxwe.webp",
       link: "https://github.com/SayidMushtaQ/PizzaSales",
+    },  
+    {
+      title: "Unlocking Boston House Prices: What Drives Value? 🏘️",
+      description: "Dove into the Boston Housing Dataset and uncovered what shapes house prices! From more rooms and low crime boosting value to high pollution and industry dragging it down, the data tells a clear story. Check out key insights on how factors like proximity to jobs or the Charles River impact prices!",
+      image: "https://res.cloudinary.com/ddpvlh9he/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,w_0.5,y_0.18/v1744385122/ChatGPT_Image_Apr_11_2025_08_53_05_PM_1_bxhn03.png",
+      link: "https://github.com/SayidMushtaQ/BostonPriceStudy.git",
     },  
   ];
