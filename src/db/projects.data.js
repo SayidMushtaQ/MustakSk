018abfd452a44c898f140599816dc9fa6803c8c2
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2026-01-07 18:10:04 +0530

    add

--- src/db/projects.data.js
+++ src/db/projects.data.js
@@ -6,7 +6,7 @@ export const projects = [
       link: "https://github.com/SayidMushtaQ/PizzaSales",
     },  
     {
-      title: "Unlocking Boston House Prices: What Drives Value? 🏘️",
+      title: "Unlocking Boston House Prices: What Drives Value? 🏘️ ",
       description: "Dove into the Boston Housing Dataset and uncovered what shapes house prices! From more rooms and low crime boosting value to high pollution and industry dragging it down, the data tells a clear story. Check out key insights on how factors like proximity to jobs or the Charles River impact prices!",
       image: "https://res.cloudinary.com/ddpvlh9he/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,w_0.5,y_0.18/v1744385122/ChatGPT_Image_Apr_11_2025_08_53_05_PM_1_bxhn03.png",
       link: "https://github.com/SayidMushtaQ/BostonPriceStudy.git",
